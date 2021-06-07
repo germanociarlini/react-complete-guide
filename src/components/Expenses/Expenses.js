953db29aacb2b450bc8be602243859835f2053e4
Germano Ciarlini <germano.ciarlini@gmail.com>
2021-06-07 19:19:29 -0300

    Dinamically render the expenses list with filter functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,37 +4,34 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
-const Expenses = ({ items }) => {
+const Expenses = ({ expenses }) => {
   const [filterYear, setFilterYear] = useState("2021");
 
   const onApplyFilterHandler = (filterData) => {
     setFilterYear(filterData);
   };
 
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filterYear
+  );
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter value={filterYear} onApplyFilter={onApplyFilterHandler} />
-        <ExpenseItem
-          title={items[0].title}
-          amount={items[0].amount}
-          date={items[0].date}
+        <ExpensesFilter
+          value={filterYear}
+          onApplyFilter={onApplyFilterHandler}
         />
-        <ExpenseItem
-          title={items[1].title}
-          amount={items[1].amount}
-          date={items[1].date}
-        />
-        <ExpenseItem
-          title={items[2].title}
-          amount={items[2].amount}
-          date={items[2].date}
-        />
-        <ExpenseItem
-          title={items[3].title}
-          amount={items[3].amount}
-          date={items[3].date}
-        />
+        {filteredExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
       </Card>
     </div>
   );
