9f74d4be95567ab5ee0729884c62775bcf43701c
Germano Ciarlini <germano.ciarlini@gmail.com>
2021-11-28 12:59:50 -0300

    Add auth slice

--- 11-redux-react/src/components/Auth.js
+++ 11-redux-react/src/components/Auth.js
@@ -1,17 +1,50 @@
-import classes from './Auth.module.css';
+import classes from "./Auth.module.css";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { authActions } from "../store";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (email === "admin@test" && password === "123") {
+      dispatch(authActions.login());
+    }
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={emailChangeHandler}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={passwordChangeHandler}
+            />
           </div>
           <button>Login</button>
         </form>
