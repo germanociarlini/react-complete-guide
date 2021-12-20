3a41fc791649bdc1252709622b6e350d2d56b954
Germano Ciarlini <germano.ciarlini@gmail.com>
2021-12-20 16:07:13 -0300

    Add routing features

--- 14-router-practice/src/components/quotes/QuoteList.js
+++ 14-router-practice/src/components/quotes/QuoteList.js
@@ -21,7 +21,10 @@ const QuoteList = (props) => {
   const isSortingAscending = queryParams.get("sort") === "asc";
 
   const onSortHandler = () => {
-    history.push(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`);
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"}`,
+    });
   };
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
--- 14-router-practice/src/pages/QuoteDetail.js
+++ 14-router-practice/src/pages/QuoteDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import { DUMMY_QUOTES } from "./Quotes";
 
 export default function QuoteDetail() {
+  const match = useRouteMatch();
   const { quoteId } = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
@@ -16,7 +17,15 @@ export default function QuoteDetail() {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path="/quotes/:quoteId/comments" exact>
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Show Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`} exact>
         <Comments />
       </Route>
     </>
