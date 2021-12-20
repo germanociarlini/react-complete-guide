8e99251179f46405d30985f8b0bd3b2f3dd8654e
Germano Ciarlini <germano.ciarlini@gmail.com>
2021-12-20 16:19:02 -0300

    Add quote creation

--- 14-router-practice/src/pages/AddQuote.js
+++ 14-router-practice/src/pages/AddQuote.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/hooks/use-http";
+import { addQuote } from "../lib/lib/api";
 
 export default function AddQuote() {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const onAddQuoteHandler = (newQuote) => {
-    history.push("/quotes");
+    sendRequest(newQuote);
   };
 
-  return <QuoteForm onAddQuote={onAddQuoteHandler} />;
+  return (
+    <QuoteForm
+      isLoading={status === "pending"}
+      onAddQuote={onAddQuoteHandler}
+    />
+  );
 }
